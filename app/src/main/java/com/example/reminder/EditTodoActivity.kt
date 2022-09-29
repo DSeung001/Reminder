9bc300d1ba10de0b31422e6fd3eda5090f5e6f94
지승렬 <seungryeol156@gmail.com>
2022-09-29 17:34:54 +0900

    create 기능과 activity 연결

--- app/src/main/java/com/example/reminder/EditTodoActivity.kt
+++ app/src/main/java/com/example/reminder/EditTodoActivity.kt
@@ -1,8 +1,12 @@
 package com.example.reminder
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.reminder.databinding.ActivityEditTodoBinding
+import com.example.reminder.dto.Todo
+import java.text.SimpleDateFormat
 
 class EditTodoActivity : AppCompatActivity() {
     lateinit var binding: ActivityEditTodoBinding
@@ -10,7 +14,7 @@ class EditTodoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityEditTodoBinding.inflate(layoutInflater)
-        setContentView(R.layout.activity_edit_todo)
+        setContentView(binding.root)
 
         val type = intent.getStringExtra("type")
 
@@ -18,6 +22,27 @@ class EditTodoActivity : AppCompatActivity() {
             binding.btnSave.text = "추가하기"
         } else {
             binding.btnSave.text = "수정하기"
+        }
+
+        binding.btnSave.setOnClickListener{
+
+            val title = binding.etTodoTitle.text.toString()
+            val content = binding.etTodoContent.text.toString()
+            val currentDate = SimpleDateFormat("yyyy-MM-dd HH:mm").format(System.currentTimeMillis())
+
+            if(type.equals("ADD")){
+                if(title.isNotEmpty() && content.isNotEmpty()){
+                    val todo = Todo(0, title, content, currentDate, false)
+                    val intent = Intent().apply {
+                        putExtra("todo", todo)
+                        putExtra("flag", 0)
+                    }
+                    setResult(RESULT_OK, intent)
+                    finish()
+                }
+            } else {
+
+            }
         }
     }
 }
--- app/src/main/java/com/example/reminder/MainActivity.kt
+++ app/src/main/java/com/example/reminder/MainActivity.kt
@@ -5,23 +5,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.ViewModelProvider
 import com.example.reminder.databinding.ActivityMainBinding
+import com.example.reminder.dto.Todo
+import com.example.reminder.viewmodel.TodoViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding: ActivityMainBinding
 
-    private val requestActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){}
+    lateinit var binding: ActivityMainBinding
+    lateinit var todoViewModel: TodoViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        todoViewModel = ViewModelProvider(this)[TodoViewModel::class.java]
+
         binding.fabAdd.setOnClickListener{
             var intent = Intent(this, EditTodoActivity::class.java).apply {
                 putExtra("type", "ADD")
             }
             requestActivity.launch(intent)
+        }
+    }
+
+    private val requestActivity = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        if(it.resultCode == RESULT_OK){
+            val todo = it.data?.getSerializableExtra("todo") as Todo
+
+            when(it.data?.getIntExtra("flag",-1)){
+                0->{
+                    CoroutineScope(Dispatchers.IO).launch {
+                        todoViewModel.insert(todo)
+                    }
+                    Toast.makeText(this, "추가되었습니다", Toast.LENGTH_SHORT).show()
+                }
+            }
         }
     }
 }

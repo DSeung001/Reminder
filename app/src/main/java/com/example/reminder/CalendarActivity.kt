2f1eae7e76300203220d957205d588c798c6c3c6
지승렬 <seungryeol156@gmail.com>
2022-11-16 17:11:25 +0900

    Readme 수정 및 더미 코드 제거

--- app/src/main/java/com/example/reminder/CalendarActivity.kt
+++ app/src/main/java/com/example/reminder/CalendarActivity.kt
@@ -8,7 +8,6 @@ import com.example.reminder.adapter.CalendarAdapter
 import com.example.reminder.databinding.ActivityCalendarBinding
 import com.example.reminder.decorator.*
 import com.example.reminder.factory.ViewModelFactory
-import com.example.reminder.repository.TodoRepository
 import com.example.reminder.viewmodel.TodoViewModel
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import java.text.SimpleDateFormat
@@ -43,8 +42,6 @@ class CalendarActivity : AppCompatActivity() {
         val dateArray = intent.getStringArrayExtra("dateArray", )
         val countArray = intent.getStringArrayExtra("countArray", )
         var currentTimeString = Calendar.DATE.toString()
-        var addTextCalendarDays : List<CalendarDay> = listOf()
-        var countMap = mutableMapOf<String,String>()
 
         if (dateArray != null && countArray != null) {
             var index = 0
@@ -54,18 +51,10 @@ class CalendarActivity : AppCompatActivity() {
                 val month = splitDate[1].toInt()-1
                 val date = splitDate[2].toInt()
 
-                addTextCalendarDays += CalendarDay.from(year, month, date)
-
-                /*
-                * Decorator 에 CalendarDay 리스트 및 Map<String,String> 을 보내서 해결하려 했으나
-                * shouldDecorated 와 decorate 가 무조건 같이 실행이 안됨
-                * */
-                binding.calendarView.addDecorator(HighlightingDecorator(
+                binding.calendarView.addDecorator(setTextDecorator(
                     Collections.singleton(CalendarDay.from(year, month, date)),
                     countArray[index]
                 ))
-
-                countMap[date.toString()] = countArray[index]
                 index++
             }
         }
@@ -86,10 +75,8 @@ class CalendarActivity : AppCompatActivity() {
         binding.rvTodoList.adapter = calendarAdapter
 
         binding.calendarView.setDateSelected(currentCalendar, true)
-//        binding.calendarView.addDecorator(AddTextDecorator(addTextCalendarDays, countMap))
         binding.calendarView.addDecorator(TodayDecorator())
         binding.calendarView.addDecorator(SundayDecoraotr())
         binding.calendarView.addDecorator(SaturdayDecorator())
-
     }
 }
--- app/src/main/java/com/example/reminder/decorator/setTextDecorator.kt
+++ app/src/main/java/com/example/reminder/decorator/setTextDecorator.kt
@@ -1,13 +1,11 @@
 package com.example.reminder.decorator
 
-import android.graphics.Color
 import com.example.reminder.decorator.span.AddTextToDates
 import com.prolificinteractive.materialcalendarview.CalendarDay
 import com.prolificinteractive.materialcalendarview.DayViewDecorator
 import com.prolificinteractive.materialcalendarview.DayViewFacade
-import com.prolificinteractive.materialcalendarview.spans.DotSpan
 
-class HighlightingDecorator(dates: Collection<CalendarDay>, count:String): DayViewDecorator {
+class setTextDecorator(dates: Collection<CalendarDay>, count:String): DayViewDecorator {
 
     var dates: HashSet<CalendarDay> = HashSet(dates)
     var count = count
